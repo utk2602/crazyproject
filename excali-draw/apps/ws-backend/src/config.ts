8f0c0521fd903f5bfb572509b6b6132fca6390f4
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-07-12 22:14:39 +0530

    dependency check done

--- excali-draw/apps/ws-backend/src/config.ts
+++
@@ -1,2 +0,0 @@
-export const JWT_SECRET ="123456";
-
--- excali-draw/apps/ws-backend/src/index.ts
+++ excali-draw/apps/ws-backend/src/index.ts
@@ -1,10 +1,30 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from './config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection',function connection(ws){
-    ws.on('error',console.error);
-    ws.on('message',function message(data){
+wss.on('connection', function connection(ws, request) {
+  const url = request.url; // ws://localhost:3000?token=123123
+  // ["ws://localhost:3000", "token=123123"]
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token') || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if(typeof decoded === 'string') {
+    ws.close();
+    return;
+  }
+
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
+  ws.on('message', function message(data) {
     ws.send('pong');
-    });
-})
+  });
+
+});
